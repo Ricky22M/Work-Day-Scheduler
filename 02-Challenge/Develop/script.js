b485e27f4fe9a19f2acacfd513e4fcc82b700ccf
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-09-17 01:49:58 -0600

    added boxes to HTML and changed the current date code in JS

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,12 +1,4 @@
-// This code is being used to show the user's current date and time at the top of the page
-var timeDisplay = $('#currentDay');
+// This code is being used to show the user's current date at the top of the page
+var currentDate = moment().format('dddd, MMMM Do');
+$('#currentDay').text(currentDate);
 
-function displayCurrentDate() {
-    var currentDate = moment().format('dddd, MMMM Do h:mm:ss a');
-    timeDisplay.text(currentDate);
-}
-
-
-
-// This code is used to call the function displayCurrentDate to get the user's current date and time to updated the time every second
-setInterval(displayCurrentDate, 1000);
