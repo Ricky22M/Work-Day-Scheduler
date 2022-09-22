fee5ddc57ae4daf5e96403bb2364347bcac9a8cd
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-09-21 18:34:09 -0600

    fixed HTML and added some JS

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,4 +1,30 @@
+// Creating variables for later use
+// need to get ids of text areas to save on to local storage
+var textDescription = document.querySelector('saveBtn').addEventListener("click", function() 
+    {
+        var savedText = document.querySelector('description').value;
+        // Local Storage [User] = user
+        localStorage.setItem('savedText', text);
+        console.log("querySelector is being tested")
+    }, false
+);
+
 // This code is being used to show the user's current date at the top of the page
 var currentDate = moment().format('dddd, MMMM Do');
 $('#currentDay').text(currentDate);
 
+// Local storage code
+
+if (localStorage.getItem('description')) {
+    var textDescription = JSON.parse(localStorage.getItem('description'));
+    console.log('getting description from local storage');
+} else {
+    var textDescription = [];
+    console.log('no previous text was saved');
+}
+
+function savedDescription() {
+    localStorage.setItem('savedDescription', 'false');
+
+
+}
