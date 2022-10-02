4741d37c09fd49590cec11561134892a81f2da2a
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-10-01 21:08:47 -0600

    reorganized HTML JS files

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,30 +1,37 @@
-// Creating variables for later use
-// need to get ids of text areas to save on to local storage
-var textDescription = document.querySelector('saveBtn').addEventListener("click", function() 
-    {
-        var savedText = document.querySelector('description').value;
-        // Local Storage [User] = user
-        localStorage.setItem('savedText', text);
-        console.log("querySelector is being tested")
-    }, false
-);
+/* DECLARING VARAIABES */
 
-// This code is being used to show the user's current date at the top of the page
+// Used later to save user input
+var savedButton = document.querySelector('.savedButton');
+
+// Used later to display user's current date
 var currentDate = moment().format('dddd, MMMM Do');
+
+/* CURRENT USER'S DATE */
+
+// Setting user's current date
 $('#currentDay').text(currentDate);
 
-// Local storage code
-
-if (localStorage.getItem('description')) {
-    var textDescription = JSON.parse(localStorage.getItem('description'));
+/* LOADING LOCAL STORAGE */
+if (localStorage.getItem(timeOfHour, savedText)) {
+    var textDescription = JSON.parse(localStorage.getItem(timeOfHour, savedText));
     console.log('getting description from local storage');
 } else {
     var textDescription = [];
     console.log('no previous text was saved');
 }
 
-function savedDescription() {
-    localStorage.setItem('savedDescription', 'false');
+/* SAVING LOCAL STORAGE */
 
+// This will save the textarea text to local storage on the hour row it was being saved onto
+$("savedButton").on("click", function() {
+    //This code will add a key code to save with the text in local storage
+    var timeOfHour = $(this).parent().attr("id");
 
-}
+    // This will save only the textarea text to local storage
+    var savedText = $(this).siblings(".description").val();
+
+    // Setting the keys and values into local storage
+    localStorage.setItem(timeOfHour, savedText);
+});
+
+/* TRACKING HOURS OF THE DAY */
