afd9b77f1d59e16797954336801e5e003f04bfbb
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-10-03 15:45:10 -0600

    fixed CSS save button issue and added more to local storage code

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,10 +1,9 @@
 /* DECLARING VARAIABES */
 
-// Used later to save user input
-var saveButton = document.querySelector('.saveButton');
-
 // Used later to display user's current date
 var currentDate = moment().format('dddd, MMMM Do');
+// Used later to track the user's current hour
+var userHours = moment().hour();
 
 /* CURRENT USER'S DATE */
 
@@ -13,22 +12,20 @@ $('#currentDay').text(currentDate);
 
 /* SAVING LOCAL STORAGE */
 
-// the thing that is currently happening is I am able to save the first row text to local storage but not the other rows
-
 // This will save the textarea text to local storage on the hour row it was being saved onto
-$("saveButton").on("click", function() {
+$(".saveButton").on("click", function() {   
     //This code will add a key code to save with the text in local storage
     var timeOfHour = $(this).parent().attr("id");
-
     // This will save only the textarea text to local storage
     var savedText = $(this).siblings(".description").val();
-
     // Setting the keys and values into local storage
     localStorage.setItem(timeOfHour, savedText);
 });
 
 /* LOADING LOCAL STORAGE */
 
+// This code will run as soon as the page is started
+// This code will output the values from the local storage to the textarea depending on what task and hour was being saved
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
